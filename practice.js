8e076ca3a52a940a4291c8b777fa3dc0b9acdf8e
ABTechDesign402 <arifashehzadi971@gmail.com>
2025-02-10 16:01:43 +0500

    practice questions

--- practice.js
+++ practice.js
@@ -1,8 +1,81 @@
-let str = "hello javascript";
-console.log(str);
+// Write a program that takes two numbers as input and prints their sum, difference, product, and quotient.
 
-const num = 23;
-console.log(num);
+let a = parseFloat(prompt("enter a first number"));
+let b = parseFloat(prompt("enter a second number"));
 
-var names = ["ahmed", "saba", "saira", "sara"];
-console.log(names);
+console.log("a+b = ", a+b);
+console.log("a-b = ", a-b);
+console.log("a*b = ", a*b);
+console.log("a**b=", a**b);
+
+if(b !== 0 ){
+    console.log("a/b = ", a/b);
+}else{
+    console.log("number is not divided by 0")
+}
+
+
+// Write a program that checks if a number is positive, negative, or zero.
+
+ num = parseFloat(prompt("Enter a number "));
+
+if (num===0){
+    console.log(num, "is zero")
+}else if(num < 0){
+    console.log( num, "is negative")
+}else if (num > 0 ){
+    console.log(num, "is positive")
+};
+
+
+// If we want to ensure the user enters a valid number, we can use a loop to keep asking for input until a valid number is provided:
+
+let num;
+
+while (true) {
+    num = parseFloat(prompt("Enter a number"));
+
+    if(!isNaN(num)){
+        break
+    }
+
+    alert ("invalid input! please enters a valid number")
+}
+
+if(num===0){
+    console.log ("numbe is zero")
+}else if (num > 0){
+    console.log("numbe is positive")
+}else if (num < 0){
+    console.log("number is negative")
+};
+
+
+
+// Write a program that checks if a number is even or odd.
+
+
+ num = parseFloat(prompt("Enter a number"));
+
+while (isNaN(num)){
+    num= parseFloat(prompt("invalid input! please enter a valid number"));
+}
+
+ if(num%2===0){
+    console.log(num,"is even")
+}else{
+    console.log(num, "is odd")
+};
+
+
+// Write a program that prints numbers from 1 to 10 using a for loop.
+
+for (let i = 0; i <= 10 ; i++) {
+    console.log(i);
+    
+}
+
+// Write a program that prints numbers from 10 to 1 using a for loop.
+for(let num=10; num >=1; num--){
+    console.log(num)
+};
